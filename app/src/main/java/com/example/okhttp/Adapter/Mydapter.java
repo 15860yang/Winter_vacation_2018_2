2b68c3a118c784b006726e653fd14c0dd9d6c768
yanghao <1586076641@qq.com>
2018-02-28 17:11:38 +0800

    2018.02.28_02

--- app/src/main/java/com/example/okhttp/Adapter/Mydapter.java
+++ app/src/main/java/com/example/okhttp/Adapter/Mydapter.java
@@ -12,7 +12,6 @@ import com.example.okhttp.R;
 import com.example.okhttp.tool.OkHttpRequest;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by 杨豪 on 2018/2/27.
@@ -25,10 +24,11 @@ public class Mydapter extends RecyclerView.Adapter<Mydapter.MyViewHolder> {
     private boolean isAllscore = true;
     private String year;
 
-    private ArrayList<ArrayList<String>> lists;
+    private ArrayList<ArrayList<String>> alllists;
+    private ArrayList<ArrayList<String>> singlexueqiscorelist = new ArrayList<>();
     private Context context;
     public Mydapter(Context context){
-        this.lists = OkHttpRequest.getScore_inquiryList();
+        this.alllists = OkHttpRequest.getScore_inquiryList();
         this.context = context;
         this.isAllscore = true;
     }
@@ -36,6 +36,12 @@ public class Mydapter extends RecyclerView.Adapter<Mydapter.MyViewHolder> {
     public void setIsShowAllscore(boolean isShowAllscore,String year){
         this.year = year;
         this.isAllscore = isShowAllscore;
+        singlexueqiscorelist.clear();
+        for(ArrayList<String> strings:alllists){
+            if((strings.get(0) + "学年度第" + strings.get(1) + "学期").equals(year)){
+                singlexueqiscorelist.add(strings);
+            }
+        }
     }
 
     @Override
@@ -64,11 +70,13 @@ public class Mydapter extends RecyclerView.Adapter<Mydapter.MyViewHolder> {
     private void setSinglexueqiscore(MyViewHolder holder, int position){
         holder.score_No_01.setVisibility(View.GONE);
         holder.score_column.setVisibility(View.GONE);
-        String s = lists.get(position).get(0) + "学年度第" + lists.get(position).get(1) + "学期";
 
-        if(s.equals(year)){
-            setscore(holder,position);
-        }
+        holder.score_Subject_name.setText(singlexueqiscorelist.get(position).get(3));
+        holder.score_nature.setText(singlexueqiscorelist.get(position).get(4));
+        holder.score_credit.setText(singlexueqiscorelist.get(position).get(6));
+        holder.score_GPA.setText(singlexueqiscorelist.get(position).get(7));
+        holder.score_score.setText(singlexueqiscorelist.get(position).get(8));
+
     }
     /**
      * 设置全部成绩
@@ -79,7 +87,7 @@ public class Mydapter extends RecyclerView.Adapter<Mydapter.MyViewHolder> {
         if(position == 0){
             tochange = true;
         }else{
-            if(!lists.get(position).get(1).equals( lists.get(position-1).get(1) )){
+            if(!alllists.get(position).get(1).equals( alllists.get(position-1).get(1) )){
                 tochange = true;
             }else {
                 tochange = false;
@@ -87,8 +95,8 @@ public class Mydapter extends RecyclerView.Adapter<Mydapter.MyViewHolder> {
         }
         if(tochange){
             holder.score_No_01.setVisibility(View.VISIBLE);
-            holder.score_No_01.setText(lists.get(position).get(0) +
-                    "学年度第"+ lists.get(position).get(1)+"学期");
+            holder.score_No_01.setText(alllists.get(position).get(0) +
+                    "学年度第"+ alllists.get(position).get(1)+"学期");
             holder.score_column.setVisibility(View.VISIBLE);
         }else {
             holder.score_No_01.setVisibility(View.GONE);
@@ -103,29 +111,18 @@ public class Mydapter extends RecyclerView.Adapter<Mydapter.MyViewHolder> {
      */
     private void setscore(MyViewHolder holder,int position){
         //学年，学期，课程代码，课程名称，课程性质，课程归属，学分，绩点，成绩，辅修标记，补考成绩，重修成绩，开课学院，备注，重修标记
-        holder.score_Subject_name.setText(lists.get(position).get(3));
-        holder.score_nature.setText(lists.get(position).get(4));
-        holder.score_credit.setText(lists.get(position).get(6));
-        holder.score_GPA.setText(lists.get(position).get(7));
-        holder.score_score.setText(lists.get(position).get(8));
+        holder.score_Subject_name.setText(alllists.get(position).get(3));
+        holder.score_nature.setText(alllists.get(position).get(4));
+        holder.score_credit.setText(alllists.get(position).get(6));
+        holder.score_GPA.setText(alllists.get(position).get(7));
+        holder.score_score.setText(alllists.get(position).get(8));
     }
 
     @Override
     public int getItemCount() {
         if(isAllscore)
-            return lists.size();
-        else return getsize();
-    }
-
-    private int getsize() {
-        int i = 0;
-        for(ArrayList<String> A : lists){
-            String s = A.get(0) + "学年度第" + A.get(1) + "学期";
-            if(s.equals(year)){
-                i++;
-            }
-        }
-        return i;
+            return alllists.size();
+        else return singlexueqiscorelist.size();
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder{
@@ -142,7 +139,6 @@ public class Mydapter extends RecyclerView.Adapter<Mydapter.MyViewHolder> {
             super(itemView);
             score_No_01 = itemView.findViewById(R.id.score_No_01);
             score_column = itemView.findViewById(R.id.score_column);
-
             score_Subject_name = itemView.findViewById(R.id.score_Subject_name);
             score_nature = itemView.findViewById(R.id.score_nature);
             score_credit = itemView.findViewById(R.id.score_credit);
